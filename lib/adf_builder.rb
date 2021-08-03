388c1e2d2d613f3471d09dabb2becb93d62ac020
marcus.salinas <12.marcus.salinas@gmail.com>
2021-08-03 16:31:16 +0400

    init commit

---
+++ lib/adf_builder.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "adf_builder/version"
+
+module AdfBuilder
+  class Error < StandardError; end
+  class AdfBuilder
+    def initialize
+
+    end
+  end
+end
---
+++ spec/adf_builder_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe AdfBuilder do
+  it "has a version number" do
+    expect(AdfBuilder::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
